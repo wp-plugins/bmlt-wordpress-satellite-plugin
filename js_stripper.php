7ca7427e61acb09c84a0228d55bbe2390dd2db31
magblogapi <magblogapi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-31 15:31:40 +0000

    1.5.5 Release
    git-svn-id: https://plugins.svn.wordpress.org/bmlt-wordpress-satellite-plugin/tags/1.5.5@247566 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js_stripper.php
@@ -0,0 +1,51 @@
+<?php
+/***********************************************************************/
+/** 	\file	js_stripper.php
+
+	\brief	This file reads in a JavaScript file, and optimizes it by stripping
+	out comments and whitespace. It will also try to GZ compress the output
+	using the standard OB functions. It can make a HUGE difference in size.
+	
+	The way it works is that you call it from the <link/> element (don't
+	specify a "type" attribute), and give it a GET parameter of filename,
+	which will equal the file path to the JavaScript file.
+	
+	For security purposes, the file must always be a ".js" file, and you can't
+	go out of the directory in which this file is located.
+*/
+	$pathname = $_GET['filename'];
+	if ( !preg_match ( "|/|", $pathname ) )
+		{
+		if ( preg_match ( "|.*?\.js$|", $pathname ) )
+			{
+			$pathname = dirname ( __FILE__ )."/$pathname";
+			$opt = file_get_contents ( $pathname );
+			$opt = preg_replace( "|[\n\r]+|s", "\n", $opt );
+			$opt = preg_replace( "|\/\*.*?\*\/|s", "", $opt );
+			$opt = preg_replace( '#\/\/.*?\n#', "", $opt );
+			$opt = preg_replace( "|\t+|s", " ", $opt );
+			$opt = preg_replace( "| +|s", " ", $opt );
+			$opt = preg_replace( "|\n+|s", "\n", $opt );
+			header ( "Content-type: text/javascript" );	
+			if ( zlib_get_coding_type() === false )
+				{
+					ob_start("ob_gzhandler");
+				}
+				else
+				{
+					ob_start();
+				}
+
+				echo $opt;
+			ob_end_flush();
+			}
+		else
+			{
+			echo "FILE MUST BE A .JS FILE!";
+			}
+		}
+	else
+		{
+		echo "YOU CANNOT LEAVE THE DIRECTORY!";
+		}
+?>
